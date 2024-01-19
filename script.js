4566a09727d56c3d808e690fda4d826078244271
samarth01 <84143644+Samarth-Nanhore@users.noreply.github.com>
2024-01-19 22:06:07 +0530

    Changes in script.js
    - Create variables to store DOM elements by id names newForm, title, author, pages and read
    - Add event Listener to newForm DOM variable for submit event
    - Each submit event on newForm variable call function named getInputDetail
    - Write function name getInputDetail with parameter value event
    - Add  event.preventDefault() in getInputDetail function for preventing submission of form to server, create variables to store values of form elements, and at last on each submission of form to reset it add .reset().

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ function Book(title, author, pages, read) {
   this.pages = pages;
   this.read = read;
 }
+
+// DOM elements
+let newForm = document.getElementById("newForm");
+let titleInputElement = document.getElementById("title");
+let authorInputElement = document.getElementById("author");
+let pagesInputElement = document.getElementById("pages");
+let readInputElement = document.getElementById("read");
+
+newForm.addEventListener("submit", getInputDetail);
+
+function getInputDetail(event) {
+  event.preventDefault();
+  let title = titleInputElement.value;
+  let author = authorInputElement.value;
+  let pages = pagesInputElement.value;
+  let read = readInputElement.value;
+  console.log(title, author, pages, read);
+  newForm.reset();
+}
