9936f52d279bc16add1035152b0966b3af004a80
samarth01 <84143644+Samarth-Nanhore@users.noreply.github.com>
2024-01-27 02:23:06 +0530

    Changes in index.html and script.js
    - Clear temp card html elements from index.html
    - Write function name createCard in script.js, as name suggest to create new card and it's elements in div element with class name cards-container

--- script.js
+++ script.js
@@ -42,3 +42,54 @@ let updateObject = (arr) => {
     return { id: index + 1, ...book };
   });
 };
+
+//DOM target variable
+const cardContainerElement = document.querySelector("div.cards-container");
+
+// DOM createElement variable
+let oneCard;
+let titleContainer;
+let authorContainer;
+let pageContainer;
+let readContainer;
+let btnContainer;
+
+let titleValue;
+let authorValue;
+let pageValue;
+let readValue;
+
+let createCard = () => {
+  oneCard = document.createElement("div");
+  oneCard.classList.add("card"); // from style css
+  cardContainerElement.appendChild(oneCard);
+
+  titleContainer = document.createElement("div");
+  authorContainer = document.createElement("div");
+  pageContainer = document.createElement("div");
+  readContainer = document.createElement("div");
+  btnContainer = document.createElement("div");
+  btnContainer.classList.add("card-btn");
+
+  oneCard.appendChild(titleContainer);
+  oneCard.appendChild(authorContainer);
+  oneCard.appendChild(pageContainer);
+  oneCard.appendChild(readContainer);
+  oneCard.appendChild(btnContainer);
+
+  titleContainer.innerHTML = "<p>Title:</p>";
+  titleValue = document.createElement("p");
+  titleContainer.appendChild(titleValue);
+
+  authorContainer.innerHTML = "<p>Author:</p>";
+  authorValue = document.createElement("p");
+  authorContainer.appendChild(authorValue);
+
+  pageContainer.innerHTML = "<p>Pages:</p>";
+  pageValue = document.createElement("p");
+  pageContainer.appendChild(pageValue);
+
+  readContainer.innerHTML = "<p>Have you read it?</p>";
+  readValue = document.createElement("p");
+  readContainer.appendChild(readValue);
+};
